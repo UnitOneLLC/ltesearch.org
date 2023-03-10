56e7f7248954fec0124f48351fe965f9ea24ba4d
Frederick Hewett <fred.hewett@gmail.com>
2023-03-10 09:58:10 -0500

    fix facebook in editor

--- editor/editor.js
+++ editor/editor.js
@@ -5,7 +5,7 @@ var USE_TEST_DATA = false;
 const READER_URL = "https://ltesearch.org/read";
 const DRAFT_URL = "https://ltesearch.org/draft";
 const TWITTER_URL = "https://twitter.com/intent/tweet";
-const FACEBOOK_URL = "https://www.facebook.com/dialog/share?app_id=80401312489";
+const FACEBOOK_URL = "https://www.facebook.com/share.php?u=";
 
 var gDataTable = null;
 
@@ -136,7 +136,7 @@ function makeTwitterUrl(u, title) {
 }
 
 function makeFacebookUrl(u, title) {
-    return FACEBOOK_URL + "&href=" + encodeURIComponent(u);
+    return FACEBOOK_URL + encodeURIComponent(u);
 }
 
 function buildResultTable(jsonArr) {
