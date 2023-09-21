7711f4c0db71ae7fec8dc57fb750f455033ec6d4
UnitOneLLC <fred.hewett@gmail.com>
2023-09-21 16:18:30 -0400

    parameterize docs

---
+++ userdoc/350ma/index.php
@@ -0,0 +1,11 @@
+<?php
+  define("LOGOCLASS","logo-350");
+  define("IMGPATH","350ma");
+  define("LOGONAME","350-logo.png");
+  define("WGNAME", "");
+  define("GROUPEMAIL","350ma-cambridge-media-team@googlegroups.com");
+  define("GROUPHOMEPAGE","https://groups.google.com/g/350ma-cambridge-media-team");
+  define("EMAILSUBJLINE","350MA LTE Team");
+  
+  include ("../common/index.php")
+?>
